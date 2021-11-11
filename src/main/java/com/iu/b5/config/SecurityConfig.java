d8a74e026e20ffe44150fad4d118cd20bd207c03
DANGHYEON <rlaalsrl3023@naver.com>
2021-11-11 11:49:58 +0900

    r

---
+++ src/main/java/com/iu/b5/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.iu.b5.config;
+
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		
+		
+		
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/iu/b5/member/RoleVO.java
@@ -0,0 +1,14 @@
+package com.iu.b5.member;
+
+import lombok.Data;
+
+@Data
+public class RoleVO {
+	
+	private Integer num;
+	private String roleName;
+	
+	
+	
+
+}
